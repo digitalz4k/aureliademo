fe79d2664c014a496a0e2c6174c7a564839011c8
Leonardo da Silva <leodsirs@gmail.com>
2016-05-20 00:47:12 +0200

    pushstate enabled

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
-export class Layout {
+export class App {
 
     configureRouter(config, router){
         config.title = 'Aurelia Demo';
+        config.options.pushState = true;
+        config.options.hashChange = false;
         config.map([
             { route: ['', 'hello'], moduleId: './components/hello/hello',
                 name: 'Hello', title: 'Hello', nav: true },
--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import 'foundation-sites';
 
 export function configure(aurelia) {
     aurelia.use.standardConfiguration().developmentLogging();
-    aurelia.start().then(a=> a.setRoot("layout"))
+    aurelia.start().then(a=> a.setRoot("app"))
         .then(a=> {
             $(document).foundation();
     });
